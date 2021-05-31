6039a61f9b9db846ab40e118b841092fd8de19e3
hilmicantaskiran <hlmtaskiran76@gmail.com>
2021-06-01 00:10:32 +0300

    add/comment area

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,31 @@
 import '../styles/global.css'
 import Head from 'next/head'
 import Header from '../components/header'
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="antialiased text-white relative min-h-screen bg-gray-900">
-
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/icons/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/icons/favicon/safari-pinned-tab.svg" color="#5bbad5" />
-        <title>Hilmi Can Taşkıran</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-
-      <Header />
-      <main className="">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+      redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
+      <div className="antialiased text-white relative min-h-screen bg-gray-900">
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/icons/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/icons/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/icons/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+          <title>Hilmi Can Taşkıran</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Header />
+        <main className="">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </Auth0Provider>
   )
 }
 
--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -1,8 +1,10 @@
 import { getMdxNode, getMdxPaths } from 'next-mdx/server'
 import { useHydrate } from 'next-mdx/client'
 import { mdxComponents } from '../../components/mdx-components'
+import { useAuth0 } from '@auth0/auth0-react'
 
 export default function PostPage({ post }) {
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0()
   const content = useHydrate(post, {
     components: mdxComponents
   })
@@ -13,8 +15,49 @@ export default function PostPage({ post }) {
         <h1 className="text-3xl md:text-4xl font-bold mt-10">{post.frontMatter.title}</h1>
         <p className="my-4">{post.frontMatter.excerpt}</p>
         <hr className="my-4" />
-        <div className="prose-white pb-10">{content}</div>
+        <div className="prose-white pb-6">{content}</div>
       </article>
+      
+      <form>
+        <textarea rows="2" className="border rounded w-full block px-2 py-2 focus:outline-none focus:ring bg-gray-400 focus:bg-gray-100 focus:border-blue-500 text-gray-900 placeholder-gray-900" placeholder="What are your thoughts?" />
+        <div className="mt-4 pb-10">
+          {isAuthenticated ? (
+            <div className="flex relative items-center space-x-2">
+              <div className="">
+                <button className="bg-blue-800 hover:bg-green-600 px-4 py-1 rounded">
+                  Send
+                </button> 
+              </div>
+              <div className="flex absolute right-0 space-x-2">
+                <img src={user.picture} width={24} height={24} className="rounded-full" />
+                <span>{user.name}</span>
+                <button
+                  typeof="button"
+                  className="bg-blue-800 hover:bg-red-600 rounded"
+                  onClick={() => 
+                    logout({ returnTo: process.env.NEXT_PUBLIC_URL + "/blog" })
+                  }
+                >
+                  <img className="" alt="" src="/icons/close.svg"></img>
+                </button>
+              </div>
+              
+            </div>
+          ) : (
+            <div>
+                <button 
+                  typeof="button"
+                  className="bg-blue-800 px-4 py-1 rounded"
+                  onClick={() => 
+                    loginWithRedirect({ returnTo: process.env.NEXT_PUBLIC_URL + "/blog" })
+                  }
+                > 
+                  Login 
+                </button>
+            </div>
+          )}
+        </div>
+      </form>
     </div>
   )
 }
